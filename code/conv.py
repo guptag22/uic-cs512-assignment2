1040d3b57f49eaab558b3cae2845b819a35e6564
Shubham Singh <shubham12101@iiitd.ac.in>
2020-03-09 17:27:47 -0500

    add kernel as module parameter

--- code/conv.py
+++ code/conv.py
@@ -21,9 +21,10 @@ class Conv(nn.Module):
         self.stride = stride
         self.padding = padding
         if kernel_tensor is None:
-            self.kernel = torch.randint(0, 2, (self.kernel_size, self.kernel_size))
+            # TODO: check if kernel is to be initialized from a distribution
+            self.kernel = nn.Parameter(torch.randint(0, 2, (self.kernel_size, self.kernel_size), dtype=torch.float))
         else:
-            self.kernel = kernel_tensor
+            self.kernel = nn.Parameter(kernel_tensor)
         self.padding_layer = nn.ZeroPad2d(self.kernel_size//2)
 
     def forward(self, image):
@@ -47,8 +48,8 @@ class Conv(nn.Module):
                 for i in range(padded_image.shape[2]-stop):
                     for j in range(padded_image.shape[3]-stop):
                         this_padded_image_view = this_padded_image[i:i+self.kernel_size, j:j+self.kernel_size]
-                        conv_image.append(torch.sum(this_padded_image_view * self.kernel).item())
-                conv_image = torch.tensor(conv_image)
+                        conv_image.append(torch.sum(torch.mul(this_padded_image_view, self.kernel)))
+                conv_image = torch.stack(conv_image)
                 conv_image = torch.reshape(conv_image, target_shape)
                 conv_image = torch.unsqueeze(conv_image, 0)
             conv_images[n] = conv_image
@@ -65,19 +66,19 @@ class Conv(nn.Module):
         """
 
 
-def get_torch_conv(image, kernel):
-    return F.conv2d(image, kernel)
+def get_torch_conv(image, kernel, padding=0):
+    return F.conv2d(image, kernel, padding=padding)
 
 
 if __name__ == "__main__":
-    image = torch.tensor([[1,1,1,0,0],[0,1,1,1,0],[0,0,1,1,1],[0,0,1,1,0],[0,1,1,0,0]])
+    image = torch.tensor([[1,1,1,0,0],[0,1,1,1,0],[0,0,1,1,1],[0,0,1,1,0],[0,1,1,0,0]], dtype=torch.float)
     image = torch.unsqueeze(image, 0)
     image = torch.unsqueeze(image, 0)
-    kernel = torch.tensor([[1, 0, 1],[0, 1, 0],[1, 0, 1]])
-    conv = Conv(3, kernel_tensor=kernel, padding=False)
+    kernel = torch.tensor([[1, 0, 1],[0, 1, 0],[1, 0, 1]], dtype=torch.float)
+    conv = Conv(3, padding=True, kernel_tensor=kernel)
     print(conv(image))
-    
-    # # check with PyTorch implementation
-    # kernel = torch.unsqueeze(kernel, 0)
-    # kernel = torch.unsqueeze(kernel, 0)
-    # print(get_torch_conv(image, kernel))
+
+    # check with PyTorch implementation
+    kernel = torch.unsqueeze(kernel, 0)
+    kernel = torch.unsqueeze(kernel, 0)
+    print(get_torch_conv(image, kernel, 1))
