755e364921ee932e974bf670b96996ee37118f20
Shubham Singh <shubham12101@iiitd.ac.in>
2020-03-09 15:08:13 -0500

    compare with PyTorch conv operation

--- code/conv.py
+++ code/conv.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Conv(nn.Module):
@@ -19,12 +20,10 @@ class Conv(nn.Module):
         self.kernel_size = kernel_size
         self.stride = stride
         self.padding = padding
-        # TODO: Initialize the kernel
         if kernel_tensor is None:
             self.kernel = torch.randint(0, 2, (self.kernel_size, self.kernel_size))
         else:
             self.kernel = kernel_tensor
-        # print("kernel :\n", self.kernel)
         self.padding_layer = nn.ZeroPad2d(self.kernel_size//2)
 
     def forward(self, image):
@@ -36,10 +35,7 @@ class Conv(nn.Module):
             padded_image = self.padding_layer(image)
         else:
             padded_image = image
-        # print('padded image: \n', padded_image)
-        # print('padded image shape: ', padded_image.shape)
         target_shape = padded_image.shape[-1]-((self.kernel_size//2)+1), padded_image.shape[-2]-((self.kernel_size//2)+1)
-        # print('target shape: ', target_shape)
         stop = (self.kernel_size//2) + 1
 
         conv_images = torch.zeros((image.shape[0], image.shape[1], target_shape[0], target_shape[1]))
@@ -50,19 +46,13 @@ class Conv(nn.Module):
                 # TODO: check the upper range bound for different kernel size
                 for i in range(padded_image.shape[2]-stop):
                     for j in range(padded_image.shape[3]-stop):
-                        # print(i+1, j+1)
                         this_padded_image_view = this_padded_image[i:i+self.kernel_size, j:j+self.kernel_size]
                         conv_image.append(torch.sum(this_padded_image_view * self.kernel).item())
                 conv_image = torch.tensor(conv_image)
                 conv_image = torch.reshape(conv_image, target_shape)
-                # print(conv_image.shape)
                 conv_image = torch.unsqueeze(conv_image, 0)
-                # TODO: stack conv images
             conv_images[n] = conv_image
-        # print(conv_images.shape)
         return conv_images
-
-
                         
 
     # def backward(self):
@@ -75,16 +65,19 @@ class Conv(nn.Module):
         """
 
 
+def get_torch_conv(image, kernel):
+    return F.conv2d(image, kernel)
+
 
 if __name__ == "__main__":
     image = torch.tensor([[1,1,1,0,0],[0,1,1,1,0],[0,0,1,1,1],[0,0,1,1,0],[0,1,1,0,0]])
-    # k = torch.tensor([[1, 0, 1],[0, 1, 0],[1, 0, 1]])
-    # image = image[:3, :3]
-    # print(image.shape)
-    # print(image * k)
     image = torch.unsqueeze(image, 0)
     image = torch.unsqueeze(image, 0)
-    filter = torch.tensor([[1, 0, 1],[0, 1, 0],[1, 0, 1]])
-    # print(image.shape)
-    conv = Conv(3, kernel_tensor=filter)
+    kernel = torch.tensor([[1, 0, 1],[0, 1, 0],[1, 0, 1]])
+    conv = Conv(3, kernel_tensor=kernel, padding=False)
     print(conv(image))
+    
+    # # check with PyTorch implementation
+    # kernel = torch.unsqueeze(kernel, 0)
+    # kernel = torch.unsqueeze(kernel, 0)
+    # print(get_torch_conv(image, kernel))
